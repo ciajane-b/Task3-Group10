6059c114988b5b7d3ed470a68695ee3ace13c515
AldCream <aldrianmaure@gmail.com>
2026-02-19 18:51:12 +0800

    Add constructor for Student class using Builder pattern

--- Student.java
+++ Student.java
@@ -12,5 +12,18 @@ public class Student {
     private final String contactNumber;
     private final String email;
     private final String course;
-  
+
+  private Student(Builder builder) {
+        this.id = builder.id;
+        this.firstName = builder.firstName;
+        this.middleName = builder.middleName;
+        this.lastName = builder.lastName;
+        this.age = builder.age;
+        this.gender = builder.gender;
+        this.address = builder.address;
+        this.contactNumber = builder.contactNumber;
+        this.email = builder.email;
+        this.course = builder.course;
+    }
+
 }
